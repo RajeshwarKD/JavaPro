76839e404b42cf834bddb002c7fcb1ab8671d5fa
Rajeshwar Dewangan <84184773+RajeshwarKD@users.noreply.github.com>
2022-03-22 20:33:37 +0530

    Add files via upload

---
+++ multiThread/multiThreadRunnableInterface.java
@@ -0,0 +1,32 @@
+package com.company;
+
+class MyThreadRunnable1 implements Runnable {
+    public void run() {
+        int i = 0;
+        while (i < 40000) {
+            System.out.println("I am firing gun1 with bullets1");
+            i++;
+        }
+    }
+}
+class MyThreadRunnable2 implements Runnable {
+    public void run() {
+        int i = 0;
+        while (i < 40000) {
+            System.out.println("I am firing gun2 with bullets2");
+            i++;
+        }
+    }
+}
+public class multiThreadRunnableInterface {
+    public static void main(String[] args) {
+        MyThreadRunnable1 bullet1 = new MyThreadRunnable1();
+        Thread gun1 = new Thread(bullet1);
+
+        MyThreadRunnable2 bullet2 = new MyThreadRunnable2();
+        Thread gun2 = new Thread(bullet2);
+
+        gun1.start();
+        gun2.start();
+    }
+}
