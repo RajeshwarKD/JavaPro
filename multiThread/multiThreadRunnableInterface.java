b1df8143d7c4c3d63b7d2fe68d12826fcdbc1454
Shubham Mohanty <101620532+ShubhamMohanty680@users.noreply.github.com>
2022-03-24 11:06:17 +0530

    Update multiThreadRunnableInterface.java

--- multiThread/multiThreadRunnableInterface.java
+++ multiThread/multiThreadRunnableInterface.java
@@ -3,7 +3,7 @@ package com.company;
 class MyThreadRunnable1 implements Runnable {
     public void run() {
         int i = 0;
-        while (i < 40000) {
+        while (i < 40) {
             System.out.println("I am firing gun1 with bullets1");
             i++;
         }
@@ -12,7 +12,7 @@ class MyThreadRunnable1 implements Runnable {
 class MyThreadRunnable2 implements Runnable {
     public void run() {
         int i = 0;
-        while (i < 40000) {
+        while (i < 40) {
             System.out.println("I am firing gun2 with bullets2");
             i++;
         }
