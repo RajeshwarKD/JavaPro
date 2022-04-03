9156fb9f6313cd7a0fcbfe28cb8992f139582a84
Rajeshwar Dewangan <84184773+RajeshwarKD@users.noreply.github.com>
2022-04-03 23:21:19 +0530

    Add files via upload

---
+++ fileHandling or InputOutput Stream/fileHandling.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class fileHandling {
+    public static void main(String[] args) {
+        // Code to create a new file
+
+       /* File myFile = new File("fileHandling.txt");
+        try {
+            myFile.createNewFile();
+            System.out.println("File is created successfully");
+        } catch (IOException e) {
+            System.out.println("Unable to create this file");
+            e.printStackTrace();
+        }
+        */
+
+        // Code to write to a file
+      /* try {
+            FileWriter fileWriter = new FileWriter("fileHandling.txt");
+            fileWriter.write("This is our first file from this java subject\nPlease insert all text string");
+            fileWriter.close();
+            System.out.println("File is writing successfully");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } */
+
+       // Reading a file
+       /* File fileReading = new File("fileHandling.txt");
+        try {
+            Scanner sc = new Scanner(fileReading);
+            while(sc.hasNextLine()){
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } */
+
+        // Deleting a file
+        File myFile = new File("fileHandling.txt");
+        if(myFile.delete()){
+            System.out.println("I have deleted: " + myFile.getName());
+        }
+        else{
+            System.out.println("Some problem occurred while deleting the file");
+        }
+    }
+}
