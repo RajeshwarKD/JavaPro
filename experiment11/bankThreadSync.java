f64376c55f9475af643c4b75c06b7fd3e454331b
Rajeshwar Dewangan <84184773+RajeshwarKD@users.noreply.github.com>
2022-04-15 23:01:36 +0530

    Update bankThreadSync.java

--- experiment11/bankThreadSync.java
+++ experiment11/bankThreadSync.java
@@ -3,73 +3,72 @@ package com.company;
 class bankAccount {
     int balance= 1000;
 
-    public int getBal(){
+    public int getBalance(){
         return balance;
     }
 
-    public void withdraw(int bal){
-       balance= balance-bal;
+    public void withdraw(int amount){
+       balance= balance-amount;
     }
 
-    public void deposit(int bal){
-        balance= balance+bal;
+    public void deposit(int amount){
+        balance= balance+amount;
     }
 }
 
 public class bankThreadSync implements Runnable{
 
-    bankAccount acc = new bankAccount();
+    bankAccount jointAccount = new bankAccount();
 
-    public static void main(String[] args) {
-        bankThreadSync ts = new bankThreadSync();
-        Thread t1 = new Thread(ts, "person 1");
-        Thread t2 = new Thread(ts, "person 2");
-        Thread t3 = new Thread(ts, "person 3");
-        t1.start();
-        t2.start();
-        t3.start();
-    }
-
-    @Override
+   @Override
     public void run() {
-        for (int i = 0; i <= 3; i++) {
+        for (int i = 3; i <= 3; i++) {
             withdraw(100);
-            if (acc.getBal() < 0) {
+            if (jointAccount.getBalance() < 0) {
                 System.out.println("account is overdrawn!");
             }
             deposit(200);
         }
     }
 
-
-    private void withdraw(int bal){
-        if (acc.getBal()>=bal) {
-            System.out.println(Thread.currentThread().getName()+" "+ "is try to withdraw Rs." + bal);
+    private synchronized void withdraw(int amount){
+        if (jointAccount.getBalance()>=amount) {
+            jointAccount.withdraw(amount);
+            System.out.println(Thread.currentThread().getName()+" "+ "is complete the withdraw of Rs." + amount);
+            System.out.println(Thread.currentThread().getName()+" after withdrawal joint account balance Rs. " + jointAccount.getBalance());
             try {
                 Thread.sleep(100);
-            } catch (Exception e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            acc.withdraw(bal);
-            System.out.println(Thread.currentThread().getName()+" "+ "is complete the withdraw of Rs." + bal);
         }else{
-            System.out.println(Thread.currentThread().getName()+ " "+"doesn't have enough money for withdraw ");
+            System.out.println(Thread.currentThread().getName()+ " "+"doesn't have enough amount for withdraw ");
         }
     }
 
-    private void deposit(int bal){
-        if (bal>=0) {
-            System.out.println(Thread.currentThread().getName()+" "+ " is try to deposit Rs." + bal);
+    private synchronized void deposit(int amount){
+        if (amount>=0) {
+            jointAccount.deposit(amount);
+            System.out.println(Thread.currentThread().getName()+" "+ "is complete the deposited of Rs." + amount);
+            System.out.println(Thread.currentThread().getName()+" after deposit joint account balance Rs. " + jointAccount.getBalance());
             try {
                 Thread.sleep(100);
-            } catch (Exception e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            acc.deposit(bal);
-            System.out.println(Thread.currentThread().getName()+" "+ "is complete the deposit of Rs." + bal);
         }else{
-            System.out.println(Thread.currentThread().getName()+ " "+"doesn't have enough money for deposit");
+            System.out.println(Thread.currentThread().getName()+ " "+"you have invalid amount for deposit");
         }
+    }
+
+    public static void main(String[] args) {
+        bankThreadSync ts = new bankThreadSync();
+        Thread t1 = new Thread(ts, "Naresh");
+        Thread t2 = new Thread(ts, "Mahesh");
+        Thread t3 = new Thread(ts, "Suresh");
+        t1.start();
+        t2.start();
+        t3.start();
     }
 }
 
