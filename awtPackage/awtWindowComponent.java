ddf1d290c14168024c898959fd908ee769368c91
Rajeshwar Dewangan <84184773+RajeshwarKD@users.noreply.github.com>
2022-04-26 23:12:18 +0530

    Update awtWindowComponent.java

--- awtPackage/awtWindowComponent.java
+++ awtPackage/awtWindowComponent.java
@@ -2,51 +2,51 @@ package com.company;
 import java.awt.*;
 import java.awt.event.*;
 
-public class awtWindowComponent extends Frame {
+public class awtWindowComponent extends Frame implements WindowListener{
         awtWindowComponent(){
         setSize(300, 500);
         setLocation(200,300);
         setVisible(true);
+
+        addWindowListener(this);
     }
     public static void main(String[] args) {
         awtWindowComponent wc = new awtWindowComponent();
+    }
 
-        wc.addWindowListener(new WindowListener() {
-            @Override
-            public void windowOpened(WindowEvent e) {
-                System.out.println("window is opened");
-            }
+    @Override
+    public void windowOpened(WindowEvent e) {
+        System.out.println("window is opened");
+    }
 
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.out.println("window is closing");
-                wc.dispose(); // finally close the window
-            }
+    @Override
+    public void windowClosing(WindowEvent e) {
+        System.out.println("window is closing");
+        dispose(); // finally close the window
+    }
 
-            @Override
-            public void windowClosed(WindowEvent e) {
-                System.out.println("window is closed");
-            }
+    @Override
+    public void windowClosed(WindowEvent e) {
+        System.out.println("window is closed");
+    }
 
-            @Override
-            public void windowIconified(WindowEvent e) {
-                System.out.println("windows is minimize");
-            }
+    @Override
+    public void windowIconified(WindowEvent e) {
+        System.out.println("windows is minimize");
+    }
 
-            @Override
-            public void windowDeiconified(WindowEvent e) {
-                System.out.println("windows is maximize");
-            }
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        System.out.println("windows is maximize");
+    }
 
-            @Override
-            public void windowActivated(WindowEvent e) {
-                System.out.println("window is activated");
-            }
+    @Override
+    public void windowActivated(WindowEvent e) {
+        System.out.println("window is activated");
+    }
 
-            @Override
-            public void windowDeactivated(WindowEvent e) {
-                System.out.println("window is deactivated");
-            }
-        });
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        System.out.println("window is deactivated");
     }
 }
