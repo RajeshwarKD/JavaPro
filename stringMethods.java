0b7dd6554745de625e79665f36b955f4f7065a08
Rajeshwar Dewangan <84184773+RajeshwarKD@users.noreply.github.com>
2022-07-01 23:23:54 +0530

    Update stringMethods.java

--- stringMethods.java
+++ stringMethods.java
@@ -3,42 +3,43 @@ package com.company;
 public class stringMethods {
     public static void main(String[] args) {
         String name = "Happy";
-        // System.out.println(name);
-        //int value = name.length();
-        //System.out.println(value);
+        System.out.println(name);
 
-        //String lstring = name.toLowerCase();
-        //System.out.println(lstring);
+        int value = name.length();
+        System.out.println(value);
 
-        //String ustring = name.toUpperCase();
-        //System.out.println(ustring);
+        String lstring = name.toLowerCase();
+        System.out.println(lstring);
 
-        //String nonTrimmedString = "     Happy     ";
-        //System.out.println(nonTrimmedString);
+        String ustring = name.toUpperCase();
+        System.out.println(ustring);
 
-        //String trimmedString = nonTrimmedString.trim();
-        //System.out.println(trimmedString);
+        String nonTrimmedString = "     Happy     ";
+        System.out.println(nonTrimmedString);
 
-        //System.out.println(name.substring(1));
-        //System.out.println(name.substring(1,5));
+        String trimmedString = nonTrimmedString.trim();
+        System.out.println(trimmedString);
 
-        //System.out.println(name.replace('r', 'p'));
-        //System.out.println(name.replace("r", "ier"));
+        System.out.println(name.substring(1));
+        System.out.println(name.substring(1,5));
 
-        //System.out.println(name.startsWith("Har"));
-        //System.out.println(name.endsWith("dd"));
+        System.out.println(name.replace('p', 'r'));
+        System.out.println(name.replace("p", "ier"));
 
-        //System.out.println(name.charAt(4));
+        System.out.println(name.startsWith("Har"));
+        System.out.println(name.endsWith("dd"));
 
-        //String modifiedName = "Harryrryrry";
-        //System.out.println(modifiedName.indexOf("rry"));
-        //System.out.println(modifiedName.indexOf("rry", 4));
-        //System.out.println(modifiedName.lastIndexOf("rry", 7));
+        System.out.println(name.charAt(4));
 
-        //System.out.println(name.equals("Harry"));
-        //System.out.println(name.equalsIgnoreCase("HarRY"));
+        String modifiedName = "Harryrryrry";
+        System.out.println(modifiedName.indexOf("rry"));
+        System.out.println(modifiedName.indexOf("rry", 4));
+        System.out.println(modifiedName.lastIndexOf("rry", 7));
 
-        //System.out.println("I am escape sequence\tdouble quote");
+        System.out.println(name.equals("Harry"));
+        System.out.println(name.equalsIgnoreCase("HarRY"));
+
+        System.out.println("I am escape sequence\tdouble quote");
 
     }
 }
