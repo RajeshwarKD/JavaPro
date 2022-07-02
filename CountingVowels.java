6b4f2edc4b92dee60514e35951d1cc2c3831cd89
Rajeshwar Dewangan <84184773+RajeshwarKD@users.noreply.github.com>
2022-07-02 18:47:46 +0530

    Add files via upload

---
+++ CountingVowels.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+
+public class CountingVowels {
+    public static void main(String[] args) {
+        int count = 0;
+        System.out.println("Enter a sentence :");
+        Scanner sc = new Scanner(System.in);
+        String sentence = sc.nextLine();
+
+        for (int i=0 ; i<sentence.length(); i++){
+            char ch = sentence.charAt(i);
+            if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'){
+                count ++;
+            }
+        }
+        System.out.println("Number of vowels in the given sentence is "+count);
+    }
+}
