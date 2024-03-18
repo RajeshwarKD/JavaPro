fdc49d0f5dc62d11ee9c5539a10428773886533a
Rajeshwar Kumar Dewangan <84184773+RajeshwarKD@users.noreply.github.com>
2024-03-18 12:06:48 +0530

    Add files via upload

---
+++ experiment7/stringMethods.java
@@ -0,0 +1,44 @@
+public class stringMethods {
+    public static void main(String[] args) {
+        String name = "Happy";
+        System.out.println(name);
+
+        int value = name.length();
+        System.out.println(value);
+
+        String lstring = name.toLowerCase();
+        System.out.println(lstring);
+
+        String ustring = name.toUpperCase();
+        System.out.println(ustring);
+
+        String nonTrimmedString = "     Happy     ";
+        System.out.println(nonTrimmedString);
+
+        String trimmedString = nonTrimmedString.trim();
+        System.out.println(trimmedString);
+
+        System.out.println(name.substring(1));
+        System.out.println(name.substring(1,5));
+
+        System.out.println(name.replace('p', 'r'));
+        System.out.println(name.replace("p", "ier"));
+
+        System.out.println(name.startsWith("Har"));
+        System.out.println(name.endsWith("dd"));
+
+        System.out.println(name.charAt(4));
+
+        String modifiedName = "Harryrryrry";
+        System.out.println(modifiedName.indexOf("rry"));
+        System.out.println(modifiedName.indexOf("rry", 4));
+        System.out.println(modifiedName.lastIndexOf("rry", 7));
+
+        System.out.println(name.equals("Harry"));
+        System.out.println(name.equalsIgnoreCase("HarRY"));
+
+        System.out.println("I am escape sequence\tdouble quote");
+
+    }
+
+}
