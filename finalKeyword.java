600c07251334593535b27134253f6adab3aa9ad5
Rajeshwar Kumar Dewangan <84184773+RajeshwarKD@users.noreply.github.com>
2025-01-29 23:19:55 +0530

    Add files via upload

---
+++ finalKeyword.java
@@ -0,0 +1,9 @@
+public class finalKeyword {
+    public static void main(String[] args) {
+        // Define a constant variable PI
+        final double PI = 3.14159;
+
+        // Print the value of PI
+        System.out.println("Value of PI: " + PI);
+    }
+}
