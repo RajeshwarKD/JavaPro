e76b632c6b7606d8ab6f37b6c45ecc296d658644
Rajeshwar Kumar Dewangan <84184773+RajeshwarKD@users.noreply.github.com>
2025-01-30 12:32:37 +0530

    Add files via upload

---
+++ staticMethod.java
@@ -0,0 +1,10 @@
+class Test1 {
+    static void display(){ // Static methods
+        System.out.println("Hello i'am static method");
+    }
+}
+public class staticMethod {
+    public static void main(String[] args) {
+        Test1.display();
+    }
+}
---
+++ staticVariable.java
@@ -0,0 +1,9 @@
+class Test {
+    static int count = 10; // Static variable
+}
+
+public class staticVariable {
+    public static void main(String[] args) {
+        System.out.println(Test.count);
+    }
+}
