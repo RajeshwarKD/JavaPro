49f9abc9e73896c5319f12d1616039e3d8745fed
Rajeshwar Kumar Dewangan <84184773+RajeshwarKD@users.noreply.github.com>
2025-02-03 22:40:27 +0530

    Delete staticVariable.java

--- staticVariable.java
+++
@@ -1,9 +0,0 @@
-class Test {
-    static int count = 10; // Static variable
-}
-
-public class staticVariable {
-    public static void main(String[] args) {
-        System.out.println(Test.count);
-    }
-}
