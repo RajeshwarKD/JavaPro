01779de6b4c0c5f976e1b68a97c023490dcfe168
Rajeshwar Kumar Dewangan <84184773+RajeshwarKD@users.noreply.github.com>
2026-02-02 22:42:25 +0530

    Add files via upload

---
+++ thirtyMinutesJAVA/psvm.java
@@ -0,0 +1,28 @@
+public class psvm {
+    // Valid main method signature
+    public static void main(String[] args)
+//    public static void main(String[] arguments)
+//    static public void main(String[] args)
+//    public static void main(String []args)
+//    public static void main(String args[])
+//    public static void main(String... args)
+//    final synchronized strictfp public static void main(String[] args)
+
+    // InValid main method signature
+//    public static void Main(String[] args)
+//    public static String main(String[] args)
+//    private static void main(String[] args)
+//    public static void main(String args)
+//    public static void main([]String args)
+    {
+        System.out.println("hello world");
+
+//        for (int i=0; i<args.length; i++) {
+//            System.out.println(args[i]);
+//        }
+    }
+//    public static void main(int arg)
+//    {
+//        System.out.println("Overloaded main method with int argument");
+//    }
+}
