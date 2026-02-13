12945e579f365fdbba52655bf74f86cf4406207e
Rajeshwar Kumar Dewangan <84184773+RajeshwarKD@users.noreply.github.com>
2026-02-13 21:52:50 +0530

    Update abstractClass3.java

--- abstractClass3.java
+++ abstractClass3.java
@@ -1,3 +1,10 @@
+//1. A method without body (no implementation) is known as abstract method.
+//2. A method must always be declared in an abstract class, or we can say that if a class has an abstract method, it should be declared abstract as well.
+//3. If a regular class extends an abstract class, then the class must have to implement all the abstract methods of abstract parent class or it has to be declared abstract as well.
+//4. Abstract method in an abstract class is meant to be overridden in derived concrete classes otherwise compile time error will be thrown.
+//5. Abstract classes cannot be instantiated, means we can’t create an object of abstract class. 
+
+
 abstract class Parent{
     public Parent(){
         System.out.println("Mai Parent class ka constructor hoon");
@@ -33,3 +40,4 @@ public class abstractClass3 {
         //Child2 c2 = new Child2(); -- error
     }
 }
+
