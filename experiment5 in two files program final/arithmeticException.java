aefd4b5a4393a0cc10021f6b61efe0d45e843147
Rajeshwar Kumar Dewangan <84184773+RajeshwarKD@users.noreply.github.com>
2026-02-15 13:09:00 +0530

    Delete experiment5 in two files program final directory

--- experiment5 in two files program final/arithmeticException.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class arithmeticException {
-    public static void main(String[] args) {
-        arithmeticExceptionHandler result = new arithmeticExceptionHandler();
-        result.exceptionMassages();
-
-    }
-}
