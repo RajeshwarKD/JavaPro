c7a5fae0ffbdcaa4f7f97b914a39931a3e3e081d
Rajeshwar Kumar Dewangan <84184773+RajeshwarKD@users.noreply.github.com>
2026-02-22 23:40:39 +0530

    Delete accessModifierWithPackages/p1 directory

--- accessModifierWithPackages/p1/C.java
+++
@@ -1,9 +0,0 @@
-// Java Program to Illustrate
-// Protected Modifier
-package p1;
-// Class C
-public class C {
-    protected void display() {
-        System.out.println("Hello Friends I am Class C");
-    }
-}
--- accessModifierWithPackages/p1/Geek.java
+++
@@ -1,9 +0,0 @@
-// Java program to illustrate default modifier
-package p1;
-
-// Class Geek is having Default access modifier
-public class Geek {
-    public void display() {
-        System.out.println("Hello World!");
-    }
-}
